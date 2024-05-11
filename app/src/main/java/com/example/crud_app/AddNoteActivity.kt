fcdfbdfd132904b50d41f7e0c7ce5928597a13e9
Pawarasasmina <pawarasasmina2002@icloud.com>
2024-05-11 09:34:15 +0530

    Create part (1.0)

--- app/src/main/java/com/example/crud_app/AddNoteActivity.kt
+++ app/src/main/java/com/example/crud_app/AddNoteActivity.kt
@@ -1,31 +1,20 @@
 package com.example.crud_app
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class AddNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_note)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val button=findViewById<FloatingActionButton>(R.id.addButton)
-        button.setOnClickListener{
-            val Intent= Intent( this,AddNoteActivity::class.java)
-            startActivity(Intent)
-        }
-
-
-
     }
 }
